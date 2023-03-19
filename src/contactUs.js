f4a925ac46be65ff222d2bbff3f30a86926694b5
ahmedfarouk2 <ahmedoza12@gmail.com>
2023-03-19 04:24:38 +0200

    New contact message update

--- src/contactUs.js
+++ src/contactUs.js
@@ -1,4 +1,4 @@
-const mainContentText = `Well.. Contact us..? Hmmm I'm sure we're in Paris but where exactly I really don't think they mentioned in the movie, Sorry.`;
+const mainContentText = 'Well.. Contact us..? Hmmm I\'m sure we\'re in Paris but where exactly I really don\'t think they mentioned in the movie, Sorry.';
 const contactUsBtn = document.querySelector('#contactUs');
 const mainContentDiv = document.querySelector('.mainContent');
 contactUsBtn.addEventListener('click', () => {
