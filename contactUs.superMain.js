d82029a2f2ee13867c85b406c3529b6e13a9912a
ahmedfarouk2 <ahmedoza12@gmail.com>
2023-03-19 03:54:28 +0200

    Full project finished

---
+++ contactUs.superMain.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("#contactUs"),t=document.querySelector(".mainContent");e.addEventListener("click",(()=>{t.innerText="Well.. Contact us..? Hmmm I'm sure we're in Paris but where exactly I really don't think they mentioned in the movie, Sorry."}))})();
+//# sourceMappingURL=contactUs.superMain.js.map
