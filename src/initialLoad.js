8e430295818ba5ca985c251a0792d14d075ce19f
ahmedfarouk2 <ahmedoza12@gmail.com>
2023-03-19 02:06:05 +0200

    Initial page logic finished

--- src/initialLoad.js
+++ src/initialLoad.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable import/prefer-default-export */
-function headerInitalLoad() {
+function headerInitialLoad() {
   const mainDiv = document.querySelector('#content');
   const div = document.createElement('div');
   div.classList.add('header');
@@ -32,9 +32,19 @@ function aboutTabInitialLoad() {
   mainDiv.append(div);
 }
 
+function mainContentInitialLoad() {
+  const mainDiv = document.querySelector('#content');
+  const div = document.createElement('div');
+  div.innerText = `Remy, a resident of Paris, appreciates good food and has quite a sophisticated palate. He would love to become a chef so he can create and enjoy culinary masterpieces to his heart's delight. The only problem is, Remy is a rat. When he winds up in the sewer beneath one of Paris' finest restaurants, the rodent gourmet finds himself ideally placed to realize his dream.
+  `;
+  div.classList.add('mainContent');
+  mainDiv.append(div);
+}
+
 export default function initialLoad() {
-  headerInitalLoad();
+  headerInitialLoad();
   menuTabInitialLoad();
   homeTabInitialLoad();
   aboutTabInitialLoad();
+  mainContentInitialLoad();
 }
